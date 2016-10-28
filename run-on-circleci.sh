c80e5e8701d280520763cbf56efcc3325da322c4
belibak <spam1box@tut.by>
2016-10-28 12:04:19 +0200

    some changes

--- run-on-circleci.sh
+++ run-on-circleci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #git clone https://github.com/broadlook-technologies/Data_Optimization_Prototype.git
-git clone git@github.com:broadlook-technologies/Data_Optimization_Prototype.git
+#git clone git@github.com:broadlook-technologies/Data_Optimization_Prototype.git
 echo '' > local_settings.py
 echo '' > salesforce_settings.py
 docker build -t celery -f Dockerfile-celery .
